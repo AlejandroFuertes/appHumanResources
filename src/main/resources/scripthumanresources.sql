f6f38c7348d9b1428ce22c78bdd63686cbbcc446
AlejandroFuertes <alefuertes.96@gmail.com>
2022-12-16 13:12:06 -0300

    Se agrega collection de postman para hacer pruebas

--- src/main/resources/scripthumanresources.sql
+++ src/main/resources/scripthumanresources.sql
@@ -1,3 +1,5 @@
+/*Step 1*/
+/* Creamos base de datos y registramos comentario */
 CREATE DATABASE humanresources
     WITH
     OWNER = postgres
@@ -6,9 +8,7 @@ CREATE DATABASE humanresources
 COMMENT ON DATABASE humanresources
     IS 'database to keep a record of employees.';
 	
-/*Elimina tabla empleados*/
-drop table employee;
-
+/*Step 2*/
 /*El id de la tabla es autoincremental*/
 CREATE TABLE employee
 (
@@ -21,10 +21,12 @@ CREATE TABLE employee
     registration_date timestamp NOT NULL
 );
 
+/*Step 3*/
 /*Actualizamos la tabla agregando el campo fecha de aumento salarial la cual se dara cada 6 meses.*/
 ALTER TABLE employee
 ADD COLUMN salary_increase_date timestamp;
 
+/*Step 4*/
 /*Creamos procedure para guardar fecha de alta en el sistema de un empleado*/
 CREATE OR REPLACE PROCEDURE public.INSERT_PROC(v_name_proc text, v_email_proc text, v_city_proc text, v_zipcode_proc text, v_salary numeric)
 LANGUAGE 'plpgsql'
@@ -35,16 +37,27 @@ BEGIN
 END
 $BODY$;
 
-DROP PROCEDURE public.INSERT_PROC;
-
+/*Step 5*/
+/* Cargo un valor, comprobamos que el procedure funcione*/
 call public.INSERT_PROC('testName', 'testEmail', 'testCity', 'testZipcode', 100000.50);
 
+/*Step 6*/
+/* Consultamos que se guardo correctamente. */
 select * from employee;
-delete from employee;
 
+/*End of steps*/
 /* funcion para sumar o restar fechas */		
 select now() - interval'5 days';
 select now() - interval '5 days 3 hours';		   
 select now() - interval '5 days 3 hours 15 minutes';
 select now() + interval '6 month';
 select now() + interval '1 year';
+
+/* elimina procedure */
+DROP PROCEDURE public.INSERT_PROC;
+
+/* Elimina tabla empleados */
+drop table employee;
+
+/* Elimina datos de la tabla empleados */
+delete from employee;
